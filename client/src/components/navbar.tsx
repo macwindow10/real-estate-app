96466d4f0c57b1ce62fc81aefbe7c3232604d255
asif.raza@investmentlens.ca <asif.raza@investmentlens.ca>
2025-10-05 08:44:05 +0500

    navbar

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -9,11 +9,11 @@ function Navbar() {
       className="fixed top-0 left-0 w-full z-50 shadow-xl"
       style={{ height: `${NAVBAR_HEIGHT}px` }}
     >
-      <div className="flex justify-between items-center w-full py-3 px-8 bg-gray-700 text-white">
+      <div className="flex justify-between items-center w-full py-3 px-8 bg-primary-700 text-white">
         <div className="flex items-center gap-4 md:gap-6">
           <Link
             href="/"
-            className="cursor-pointer hover:text-gray-300"
+            className="cursor-pointer hover:text-primary-300"
             scroll={false}
           >
             <div className="flex items-center gap-3">
@@ -26,13 +26,16 @@ function Navbar() {
               />
               <div className="text-xl font-bold">
                 RENT
-                <span className="text-pink-500 font-light hover:text-gray-300">
+                <span className="text-secondary-500 font-light hover:text-primary-300">
                   IFUL
                 </span>
               </div>
             </div>
           </Link>
         </div>
+        <p className="hidden md:block text-primary-200">
+          Discover your perfect rental appartment with our advanced search.
+        </p>
       </div>
     </div>
   );
