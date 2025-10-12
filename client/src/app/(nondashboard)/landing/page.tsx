dd96e0dc70e5d227ad8181cc384cc7030db6e50f
asif.raza@investmentlens.ca <asif.raza@investmentlens.ca>
2025-10-12 22:20:55 +0500

    features section

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "./herosection";
+import FeaturesSection from "./featuressection";
 
 export const Landing = () => {
   return (
     <div>
       <HeroSection />
+      <FeaturesSection />
     </div>
   );
 };
